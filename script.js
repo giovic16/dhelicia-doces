003c8351ecfc34f6378a4063dc29c6f6355b5dac
giovic16 <giovanavictoria887@gmail.com>
2025-01-18 11:35:52 -0300

    [BUG] - Criação botão flutuante p3

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     var backToTopButton = document.querySelector('.back-to-top');
 
     window.addEventListener('scroll', function() {
-        if (window.scrollY > window.innerHeight / 2) {
+        var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+        var triggerPosition = document.querySelector('#valores').offsetTop;
+
+        if (window.innerWidth <= 768 && scrollPosition > triggerPosition) {
             backToTopButton.style.display = 'flex';
         } else {
             backToTopButton.style.display = 'none';
